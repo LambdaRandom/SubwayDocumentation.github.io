fd9ba5d1a59c8fc47a086109386ed7689535da94
LambdaRandom <76541366+LambdaRandom@users.noreply.github.com>
2023-01-08 01:54:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Get the root element
+var r = document.querySelector(':root');
+
+// Create a function for getting a variable value
+function myFunction_get() {
+  // Get the styles (properties and values) for the root
+  var rs = getComputedStyle(r);
+  // Alert the value of the --blue variable
+  alert("The value of --blue is: " + rs.getPropertyValue('--blue'));
+}
+
+var toggle = 'True'
+
+function toggleTheme() {
+    if (toggle == 'True') {
+        r.style.setProperty('--main-bg-color', '#c8c8c8');
+        r.style.setProperty('--main-content-color', '#313131');
+        r.style.setProperty('--main-title-color', '#313131');
+        toggle = 'False'
+    } else {
+        r.style.setProperty('--main-bg-color', '#313131');
+        r.style.setProperty('--main-content-color', '#e9e9e9');
+        r.style.setProperty('--main-title-color', '#e9e9e9');
+        toggle = 'True'
+    }
+}
